5013215a8600d2a51fd598822f64aa4cdcc3beab
Yaroslav Protsiuk <jaroslav.procjuk@gmail.com>
2023-07-05 21:20:52 +0300

    Переніс логіку до App та зробив Reset при сабміті форми

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,20 +1,69 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Searchbar from '../Searchbar/Searchbar';
 import ImageGallery from 'components/ImageGallery/ImageGallery';
 import css from './App.module.css';
 import PropTypes from 'prop-types';
+import getImages from 'services/api';
+import LoadMore from 'components/LoadMore/LoadMore';
+import { ThreeDots } from 'react-loader-spinner';
 
 const App = () => {
   const [searchImage, setSearchImage] = useState('');
+  const [image, setImage] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [page, setPage] = useState(1);
+  const [error, setError] = useState(null);
 
-  const handleFormSubmit = searchImage => {
-    setSearchImage(searchImage);
+  useEffect(() => {
+    const fetchImages = () => {
+      setIsLoading(true);
+
+      getImages(searchImage, page)
+        .then(response => response.json())
+        .then(data => {
+          if (data.total === 0) {
+            return alert('По вашому запиту нічого не знайдено');
+          }
+
+          setImage(prevImage => [...prevImage, ...data.hits]);
+        })
+        .catch(error => {
+          setError(error.message);
+        })
+        .finally(() => {
+          setIsLoading(false);
+        });
+    };
+
+    if (searchImage) {
+      console.log(page);
+
+      fetchImages();
+    }
+  }, [searchImage, page]);
+
+  const resetQuery = () => {
+    setImage([]);
+    setPage(1);
+    setError(null);
   };
 
+  const onButtonClick = () => {
+    setPage(prevPage => prevPage + 1);
+  };
+  const handleFormSubmit = searchImage => {
+    resetQuery();
+    setSearchImage(searchImage);
+  };
   return (
     <div className={css.App}>
+      {error && setError(error.message)}
       <Searchbar onSearchSubmit={handleFormSubmit} />
-      <ImageGallery searchImage={searchImage} />
+      {isLoading && <ThreeDots />}
+      <ImageGallery image={image} />
+      {image.length > 0 && searchImage && image.length % 12 === 0 && (
+        <LoadMore onClick={onButtonClick} />
+      )}
     </div>
   );
 };
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,48 +1,12 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import ImageGalleryItem from 'components/ImageGalleryItem/ImageGalleryItem';
-import LoadMore from 'components/LoadMore/LoadMore';
 import Modal from 'components/Modal/Modal';
-import { ThreeDots } from 'react-loader-spinner';
-import getImages from 'services/api';
 import css from 'components/ImageGallery/ImageGallery.module.css';
 import PropTypes from 'prop-types';
 
-const ImageGallery = ({ searchImage }) => {
-  console.log(searchImage);
-  const [image, setImage] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [page, setPage] = useState(1);
-  const [error, setError] = useState(null);
+const ImageGallery = ({ image }) => {
   const [activeImage, setActiveImage] = useState('');
   const [showModal, setShowModal] = useState(false);
-
-  useEffect(() => {
-    const fetchImages = () => {
-      setIsLoading(true);
-      getImages(searchImage, page)
-        .then(response => response.json())
-        .then(data => {
-          if (data.total === 0) {
-            return alert('По вашому запиту нічого не знайдено');
-          }
-
-          setImage(prevImage => [...prevImage, ...data.hits]);
-        })
-        .catch(error => {
-          setError(error.message);
-        })
-        .finally(() => {
-          setIsLoading(false);
-        });
-    };
-    if (searchImage) {
-      console.log(searchImage);
-      console.log(page);
-      fetchImages();
-    } else {
-      resetQuery();
-    }
-  }, [searchImage, page]);
 
   const toggleModal = () => {
     setShowModal(prevShowModal => !prevShowModal);
@@ -52,19 +16,8 @@ const ImageGallery = ({ searchImage }) => {
     setActiveImage(data);
   };
 
-  const onButtonClick = () => {
-    setPage(prevPage => prevPage + 1);
-  };
-
-  const resetQuery = () => {
-    setPage(1);
-    setImage([]);
-  };
-
   return (
     <>
-      {error && <h1>Помилка,спробуйте пізніше</h1>}
-      {isLoading && <ThreeDots />}
       {showModal && (
         <Modal onToggleModal={toggleModal}>
           <img src={activeImage.largeImageURL} alt={activeImage.tags} />
@@ -81,9 +34,6 @@ const ImageGallery = ({ searchImage }) => {
             />
           ))}
       </ul>
-      {image.length > 0 && searchImage && image.length % 12 === 0 && (
-        <LoadMore onClick={onButtonClick} />
-      )}
     </>
   );
 };
